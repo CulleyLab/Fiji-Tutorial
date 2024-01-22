1ebc9ea82fa20e4d6edf4c8b8f0c8c5e38b004f0
Siân Culley <34683340+superresolusian@users.noreply.github.com>
2024-01-22 19:24:25 +0000

    create helper class for opening images

---
+++ Fiji_tutorial/src/main/java/OpenImageHelper.java
@@ -0,0 +1,80 @@
+import ij.IJ;
+import ij.ImagePlus;
+import ij.WindowManager;
+
+public class OpenImageHelper {
+
+    public static ImagePlus openMitosisTif(){
+        ImagePlus imp = null;
+        String[] imageTitles = WindowManager.getImageTitles();
+
+        // no images - open from zip
+        if(imageTitles.length==0){
+            //imp = IJ.openImage("http://imagej.net/images/Spindly-GFP.zip");
+            imp = getLocalMitosisTif();
+        }
+        // one image
+        else if(imageTitles.length==1){
+            // image is mitosis image - set as imp
+            if(imageTitles[0].startsWith("mitosis")){
+                imp = WindowManager.getCurrentImage();
+            }
+            // image is not mitosis image - open from zip
+            else{
+                //imp = IJ.openImage("http://imagej.net/images/Spindly-GFP.zip");
+                imp = getLocalMitosisTif();
+            }
+        }
+        // more than one image
+        else{
+            boolean mitosisOpen = false;
+            // see if any of the images are mitosis.tif
+            for(String title:imageTitles){
+                if (title.equals("mitosis.tif")) {
+                    mitosisOpen = true;
+                    break;
+                }
+                // identify image and set as tif
+                imp = WindowManager.getImage("mitosis.tif");
+            }
+            // open from zip if not in open images.
+            if(!mitosisOpen){
+                //imp = IJ.openImage("http://imagej.net/images/Spindly-GFP.zip");
+                imp = getLocalMitosisTif();
+            }
+        }
+        return imp;
+    }
+
+    public static ImagePlus openNup60Stack(){
+        ClassLoader classLoader = Exploring_images.class.getClassLoader();
+        String imagePath = "Nup60_pombe_z-stack.tif"; // Adjust the path accordingly
+
+        // Use the class loader to load the image as a resource
+        java.net.URL imageURL = classLoader.getResource(imagePath);
+        ImagePlus imp = IJ.openImage(imageURL.toString());
+
+        return imp;
+    }
+
+    public static ImagePlus getLocalMitosisTif(){
+        ClassLoader classLoader = Exploring_images.class.getClassLoader();
+        String imagePath = "mitosis.tif"; // Adjust the path accordingly
+
+        // Use the class loader to load the image as a resource
+        java.net.URL imageURL = classLoader.getResource(imagePath);
+        ImagePlus imp = IJ.openImage(imageURL.toString());
+
+        return imp;
+    }
+
+    public static void main(String[] arg){
+        new ij.ImageJ();
+        ImagePlus imp1 = getLocalMitosisTif();
+        ImagePlus imp2 = openNup60Stack();
+
+        imp1.show();
+        imp2.show();
+
+    }
+}
